9f832a2e7f3fc8f8b0af2a29ce29182c53c33379
agong <agong@iastate.edu>
2024-02-05 22:31:38 -0600

    valentine asker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["media1.tenor.com", "media.giphy.com"],
+  },
+};
 
 export default nextConfig;
